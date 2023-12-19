e56c0eb0e32f254be071bcaff6a8b38e131cea86
lucazmartins <140116186+lucazmartins@users.noreply.github.com>
2023-12-19 10:34:51 -0300

    simple calc javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-const number1 = document.getElementById("num1").value;
-const number2 = document.getElementById("num2").value;
-console.log (number1 + number2);
+const multButtom = document.getElementById('buttonMult');
+const divButtom = document.getElementById('buttonDiv');
+
+multButtom.addEventListener("click",()=>{
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const numResult = Number(num1) * Number(num2);
+    document.getElementById("result").value = numResult;
+})
+divButtom.addEventListener("click",()=>{
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const numResult = Number(num1) / Number(num2);
+    document.getElementById("result").value = numResult;
+})
+
+
 
